e7499c3116eb80d0aeeadf7fc7ee88561e453d46
Franco Estevez <francoestevez2008@gmail.com>
2022-03-01 21:13:04 -0300

    Count from 0

--- src/components/count/index.jsx
+++ src/components/count/index.jsx
@@ -2,14 +2,22 @@ import React, { useState } from 'react';
 import './style.css';
 
 const Count = () => {
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
+    const add = () => {
+        setCount(count + 1)
+    }
+    const substract = () => {
+        if (count > 0) {
+            setCount(count - 1)
+        }
+    }
     return (
         <div className='countContainer'>
             <p>{count}</p>
-            <button onClick={() => {setCount(count + 1)}}>
+            <button onClick={add}>
                 +
             </button>
-            <button onClick={() => {setCount(count - 1)}}>
+            <button onClick={substract}>
                 -
             </button>
         </div>
